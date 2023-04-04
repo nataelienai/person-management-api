b611da7306f357d357581875e6b6266bfc7eee54
Natã Elienai <nata.elienai@gmail.com>
2023-04-04 17:59:02 -0300

    refactor: change Address service to use the mapper

--- src/main/java/io/github/nataelienai/usermanager/service/AddressService.java
+++ src/main/java/io/github/nataelienai/usermanager/service/AddressService.java
@@ -1,12 +1,13 @@
 package io.github.nataelienai.usermanager.service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
 import org.springframework.stereotype.Service;
 
 import io.github.nataelienai.usermanager.dto.AddressRequest;
+import io.github.nataelienai.usermanager.dto.AddressResponse;
+import io.github.nataelienai.usermanager.dto.mapper.AddressMapper;
 import io.github.nataelienai.usermanager.entity.Address;
 import io.github.nataelienai.usermanager.entity.Person;
 import io.github.nataelienai.usermanager.exception.AddressNotFoundException;
@@ -21,23 +22,23 @@ public class AddressService {
   private final AddressRepository addressRepository;
   private final PersonRepository personRepository;
 
-  public Address create(Long personId, AddressRequest addressRequest) {
+  public AddressResponse create(Long personId, AddressRequest addressRequest) {
     Person person = personRepository.findById(personId)
         .orElseThrow(() -> new PersonNotFoundException(personId));
 
     Address address = new Address(addressRequest.getCep(), addressRequest.getCity(), addressRequest.getStreet(),
         addressRequest.getNumber(), false, person);
 
-    return addressRepository.save(address);
+    Address savedAddress = addressRepository.save(address);
+    return AddressMapper.mapToResponse(savedAddress);
   }
 
-  public List<Address> findAllByPersonId(Long personId) {
+  public List<AddressResponse> findAllByPersonId(Long personId) {
     Person person = personRepository.findById(personId)
         .orElseThrow(() -> new PersonNotFoundException(personId));
 
     Set<Address> addresses = person.getAddresses();
-
-    return new ArrayList<>(addresses);
+    return AddressMapper.mapToResponseList(addresses);
   }
 
   public void setPersonAddressAsMain(Long personId, Long addressId) {
