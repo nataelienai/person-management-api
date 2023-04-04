9b6a5c281f238878c9ec17157e2a60ff861d987f
Natã Elienai <nata.elienai@gmail.com>
2023-04-04 17:59:11 -0300

    refactor: change Person service to use the mapper

--- src/main/java/io/github/nataelienai/usermanager/service/PersonService.java
+++ src/main/java/io/github/nataelienai/usermanager/service/PersonService.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import io.github.nataelienai.usermanager.dto.PersonRequest;
+import io.github.nataelienai.usermanager.dto.PersonResponse;
+import io.github.nataelienai.usermanager.dto.mapper.PersonMapper;
 import io.github.nataelienai.usermanager.entity.Person;
 import io.github.nataelienai.usermanager.exception.PersonNotFoundException;
 import io.github.nataelienai.usermanager.repository.PersonRepository;
@@ -15,27 +17,31 @@ import lombok.RequiredArgsConstructor;
 public class PersonService {
   private final PersonRepository personRepository;
 
-  public Person create(PersonRequest personRequest) {
+  public PersonResponse create(PersonRequest personRequest) {
     Person person = new Person(personRequest.getName(), personRequest.getDateOfBirth());
-    return personRepository.save(person);
+    Person savedPerson = personRepository.save(person);
+    return PersonMapper.mapToResponse(savedPerson);
   }
 
-  public List<Person> findAll() {
-    return personRepository.findAll();
+  public List<PersonResponse> findAll() {
+    return PersonMapper.mapToResponseList(personRepository.findAll());
   }
 
-  public Person findById(Long id) {
-    return personRepository.findById(id)
+  public PersonResponse findById(Long id) {
+    Person person = personRepository.findById(id)
         .orElseThrow(() -> new PersonNotFoundException(id));
+
+    return PersonMapper.mapToResponse(person);
   }
 
-  public Person update(Long id, PersonRequest personRequest) {
+  public PersonResponse update(Long id, PersonRequest personRequest) {
     Person person = personRepository.findById(id)
         .orElseThrow(() -> new PersonNotFoundException(id));
 
     person.setName(personRequest.getName());
     person.setDateOfBirth(personRequest.getDateOfBirth());
 
-    return personRepository.save(person);
+    Person savedPerson = personRepository.save(person);
+    return PersonMapper.mapToResponse(savedPerson);
   }
 }
