46ab165652e9fd66f101ba535f89792de0eac126
Natã Elienai <nata.elienai@gmail.com>
2023-04-04 23:02:13 -0300

    feat: change date of birth parsing to be done in Person Service

--- src/main/java/io/github/nataelienai/usermanager/dto/PersonRequest.java
+++ src/main/java/io/github/nataelienai/usermanager/dto/PersonRequest.java
@@ -1,9 +1,6 @@
 package io.github.nataelienai.usermanager.dto;
 
-import java.time.LocalDate;
-
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
@@ -18,6 +15,6 @@ public class PersonRequest {
   @NotBlank(message = "Name is required")
   private String name;
 
-  @NotNull(message = "Date of birth is required")
-  private LocalDate dateOfBirth;
+  @NotBlank(message = "Date of birth is required")
+  private String dateOfBirth;
 }
--- src/main/java/io/github/nataelienai/usermanager/service/PersonService.java
+++ src/main/java/io/github/nataelienai/usermanager/service/PersonService.java
@@ -1,5 +1,7 @@
 package io.github.nataelienai.usermanager.service;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
@@ -18,7 +20,7 @@ public class PersonService {
   private final PersonRepository personRepository;
 
   public PersonResponse create(PersonRequest personRequest) {
-    Person person = new Person(personRequest.getName(), personRequest.getDateOfBirth());
+    Person person = new Person(personRequest.getName(), parseDateOfBirth(personRequest.getDateOfBirth()));
     Person savedPerson = personRepository.save(person);
     return PersonMapper.mapToResponse(savedPerson);
   }
@@ -39,9 +41,14 @@ public class PersonService {
         .orElseThrow(() -> new PersonNotFoundException(id));
 
     person.setName(personRequest.getName());
-    person.setDateOfBirth(personRequest.getDateOfBirth());
+    person.setDateOfBirth(parseDateOfBirth(personRequest.getDateOfBirth()));
 
     Person savedPerson = personRepository.save(person);
     return PersonMapper.mapToResponse(savedPerson);
+  }
+
+  private LocalDate parseDateOfBirth(String dateOfBirth) {
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    return LocalDate.parse(dateOfBirth, formatter);
   }
 }
