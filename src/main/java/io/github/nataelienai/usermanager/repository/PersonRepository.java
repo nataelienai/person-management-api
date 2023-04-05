2d89576c16f375f2ea96a15e25b10f7f2d1cadc6
Natã Elienai <nata.elienai@gmail.com>
2023-04-05 11:33:45 -0300

    refactor: remove unnecessary @Repository annotations

--- src/main/java/io/github/nataelienai/usermanager/repository/PersonRepository.java
+++ src/main/java/io/github/nataelienai/usermanager/repository/PersonRepository.java
@@ -1,10 +1,8 @@
 package io.github.nataelienai.usermanager.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import io.github.nataelienai.usermanager.entity.Person;
 
-@Repository
 public interface PersonRepository extends JpaRepository<Person, Long> {
 }
