6e0e9754936f5e4e555d45ee1e37516f538614b6
Natã Elienai <nata.elienai@gmail.com>
2023-04-05 10:28:34 -0300

    fix: change primary key generation strategy to identity

--- src/main/java/io/github/nataelienai/usermanager/entity/Address.java
+++ src/main/java/io/github/nataelienai/usermanager/entity/Address.java
@@ -21,7 +21,7 @@ import lombok.Setter;
 @Entity
 public class Address {
   @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
   private String cep;
--- src/main/java/io/github/nataelienai/usermanager/entity/Person.java
+++ src/main/java/io/github/nataelienai/usermanager/entity/Person.java
@@ -24,7 +24,7 @@ import lombok.Setter;
 @Entity
 public class Person {
   @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
   private String name;
