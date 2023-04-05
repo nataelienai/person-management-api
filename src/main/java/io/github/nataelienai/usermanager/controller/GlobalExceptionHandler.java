46564050425eac2675b5078edb63d791ad3e1ab5
Natã Elienai <nata.elienai@gmail.com>
2023-04-05 11:07:50 -0300

    feat: add uncaught exception handling

--- src/main/java/io/github/nataelienai/usermanager/controller/GlobalExceptionHandler.java
+++ src/main/java/io/github/nataelienai/usermanager/controller/GlobalExceptionHandler.java
@@ -3,6 +3,8 @@ package io.github.nataelienai.usermanager.controller;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -18,6 +20,8 @@ import lombok.Getter;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+  private final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
   @Getter
   @AllArgsConstructor
   public class ErrorResponse {
@@ -68,5 +72,12 @@ public class GlobalExceptionHandler {
   @ResponseStatus(HttpStatus.NOT_FOUND)
   public ErrorResponse handleResourceNotFoundException(RuntimeException exception) {
     return new ErrorResponse(HttpStatus.NOT_FOUND.value(), exception.getMessage());
+  }
+
+  @ExceptionHandler(Exception.class)
+  @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+  public ErrorResponse handleUncaughtException(Exception exception) {
+    logger.error(exception.getMessage(), exception);
+    return new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Internal Server Error");
   }
 }
