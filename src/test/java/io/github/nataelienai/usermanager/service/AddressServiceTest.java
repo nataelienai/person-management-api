c9db6b2b73b2695bc8b571031892cbea8999b451
Natã Elienai <nata.elienai@gmail.com>
2023-04-05 20:21:48 -0300

    test: add tests for AddressService setPersonAddressAsMain method

--- src/test/java/io/github/nataelienai/usermanager/service/AddressServiceTest.java
+++ src/test/java/io/github/nataelienai/usermanager/service/AddressServiceTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.AdditionalAnswers.returnsFirstArg;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyIterable;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 
@@ -18,6 +19,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
@@ -25,12 +27,15 @@ import io.github.nataelienai.usermanager.dto.AddressRequest;
 import io.github.nataelienai.usermanager.dto.AddressResponse;
 import io.github.nataelienai.usermanager.entity.Address;
 import io.github.nataelienai.usermanager.entity.Person;
+import io.github.nataelienai.usermanager.exception.AddressNotFoundException;
 import io.github.nataelienai.usermanager.exception.PersonNotFoundException;
 import io.github.nataelienai.usermanager.repository.AddressRepository;
 import io.github.nataelienai.usermanager.repository.PersonRepository;
 
 @ExtendWith(MockitoExtension.class)
 class AddressServiceTest {
+  final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
   AddressService addressService;
 
   @Mock
@@ -38,6 +43,9 @@ class AddressServiceTest {
 
   @Mock
   PersonRepository personRepository;
+
+  @Captor
+  ArgumentCaptor<Set<Address>> addressesCaptor;
 
   @BeforeEach
   void setUp() {
@@ -63,12 +71,11 @@ class AddressServiceTest {
   @DisplayName("create() should save address when given a valid address request")
   void create_shouldSaveAddress_whenGivenValidAddressRequest() {
     // given
-    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
     Long personId = 1L;
     Person person = new Person();
     person.setId(personId);
     person.setName("Kendrick Lamar");
-    person.setDateOfBirth(LocalDate.parse("2000-01-01", formatter));
+    person.setDateOfBirth(LocalDate.parse("2000-01-01", DATE_FORMATTER));
     AddressRequest addressRequest = new AddressRequest("12345-123", "City", "Street", 10);
 
     given(personRepository.findById(personId)).willReturn(Optional.of(person));
@@ -108,12 +115,11 @@ class AddressServiceTest {
   @DisplayName("findAllByPersonId() should retrieve a person's addresses when their id exists")
   void findAllByPersonId_shouldRetrievePersonAddresses_whenTheirIdExists() {
     // given
-    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
     Long personId = 1L;
     Person person = new Person();
     person.setId(personId);
     person.setName("Kendrick Lamar");
-    person.setDateOfBirth(LocalDate.parse("2000-01-01", formatter));
+    person.setDateOfBirth(LocalDate.parse("2000-01-01", DATE_FORMATTER));
 
     Address address = new Address(1L, "12345-123", "City", "Street", 10, true, person);
 
@@ -129,5 +135,79 @@ class AddressServiceTest {
         .usingRecursiveComparison()
         .ignoringFields("person")
         .isEqualTo(address);
+  }
+
+  @Test
+  @DisplayName("setPersonAddressAsMain() should throw when person id does not exist")
+  void setPersonAddressAsMain_shouldThrow_whenPersonIdDoesNotExist() {
+    // given
+    Long personId = 1L;
+    Long addressId = 1L;
+    given(personRepository.findById(personId)).willReturn(Optional.empty());
+
+    // when
+    // then
+    assertThatThrownBy(() -> addressService.setPersonAddressAsMain(personId, addressId))
+        .isInstanceOf(PersonNotFoundException.class);
+  }
+
+  @Test
+  @DisplayName("setPersonAddressAsMain() should throw when person does not have the address")
+  void setPersonAddressAsMain_shouldThrow_whenPersonDoesNotHaveAddress() {
+    // given
+    Long personId = 1L;
+    Long addressId = 1L;
+    Person person = new Person();
+    person.setId(personId);
+    person.setName("Kendrick Lamar");
+    person.setDateOfBirth(LocalDate.parse("2000-01-01", DATE_FORMATTER));
+    given(personRepository.findById(personId)).willReturn(Optional.of(person));
+
+    // when
+    // then
+    assertThatThrownBy(() -> addressService.setPersonAddressAsMain(personId, addressId))
+        .isInstanceOf(AddressNotFoundException.class);
+  }
+
+  @Test
+  @DisplayName("setPersonAddressAsMain() should set address as main when person and address ids exist")
+  void setPersonAddressAsMain_shouldSetAddressAsMain_whenPersonAndAddressIdsExist() {
+    // given
+    Long personId = 1L;
+    Long addressId = 1L;
+    Person person = new Person();
+    person.setId(personId);
+    person.setName("Kendrick Lamar");
+    person.setDateOfBirth(LocalDate.parse("2000-01-01", DATE_FORMATTER));
+
+    Set<Address> addresses = Set.of(
+        new Address(addressId, "12345-123", "City", "Street", 10, false, person),
+        new Address(2L, "12345-123", "City", "Street", 10, true, person));
+    person.setAddresses(addresses);
+
+    given(personRepository.findById(personId)).willReturn(Optional.of(person));
+    given(addressRepository.saveAll(anyIterable())).willReturn(List.of());
+
+    // when
+    addressService.setPersonAddressAsMain(personId, addressId);
+
+    // then
+    then(addressRepository).should().saveAll(addressesCaptor.capture());
+    Set<Address> capturedAddresses = addressesCaptor.getValue();
+
+    assertThat(capturedAddresses).hasSize(2);
+    assertThat(capturedAddresses)
+        .usingRecursiveComparison()
+        .ignoringCollectionOrder()
+        .ignoringFields("main")
+        .isEqualTo(addresses);
+
+    for (Address address : capturedAddresses) {
+      if (addressId == address.getId()) {
+        assertThat(address.getMain()).isTrue();
+      } else {
+        assertThat(address.getMain()).isFalse();
+      }
+    }
   }
 }
