efa8130473f6da1331de335898c7c5188934918b
Natã Elienai <nata.elienai@gmail.com>
2023-04-06 21:09:25 -0300

    feat: add swagger annotations to PersonController

--- src/main/java/io/github/nataelienai/usermanager/controller/PersonController.java
+++ src/main/java/io/github/nataelienai/usermanager/controller/PersonController.java
@@ -14,38 +14,61 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.github.nataelienai.usermanager.dto.ErrorResponse;
 import io.github.nataelienai.usermanager.dto.PersonRequest;
 import io.github.nataelienai.usermanager.dto.PersonResponse;
 import io.github.nataelienai.usermanager.service.PersonService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
+@Tag(name = "Person", description = "Person resource API")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/people")
 public class PersonController {
   private final PersonService personService;
 
+  @Operation(summary = "Create a person", responses = {
+      @ApiResponse(responseCode = "201", description = "Person created"),
+      @ApiResponse(responseCode = "400", description = "Invalid request body", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+  })
   @PostMapping
   @ResponseStatus(HttpStatus.CREATED)
   public PersonResponse create(@Valid @RequestBody PersonRequest personRequest) {
     return personService.create(personRequest);
   }
 
+  @Operation(summary = "Get all people", responses = {
+      @ApiResponse(responseCode = "200", description = "People retrieved"),
+  })
   @GetMapping
   @ResponseStatus(HttpStatus.OK)
   public List<PersonResponse> findAll() {
     return personService.findAll();
   }
 
-  @GetMapping("/{id}")
+  @Operation(summary = "Get a person by their id", responses = {
+      @ApiResponse(responseCode = "200", description = "Person retrieved"),
+      @ApiResponse(responseCode = "404", description = "Person id not found", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+  })
+  @GetMapping("/{personId}")
   @ResponseStatus(HttpStatus.OK)
-  public PersonResponse findById(@PathVariable Long id) {
-    return personService.findById(id);
+  public PersonResponse findById(@PathVariable Long personId) {
+    return personService.findById(personId);
   }
 
-  @PutMapping("/{id}")
+  @Operation(summary = "Update a person by their id", responses = {
+      @ApiResponse(responseCode = "200", description = "Person updated"),
+      @ApiResponse(responseCode = "400", description = "Invalid request body", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+      @ApiResponse(responseCode = "404", description = "Person id not found", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+  })
+  @PutMapping("/{personId}")
   @ResponseStatus(HttpStatus.OK)
-  public PersonResponse update(@PathVariable Long id, @Valid @RequestBody PersonRequest personRequest) {
-    return personService.update(id, personRequest);
+  public PersonResponse update(@PathVariable Long personId, @Valid @RequestBody PersonRequest personRequest) {
+    return personService.update(personId, personRequest);
   }
 }
